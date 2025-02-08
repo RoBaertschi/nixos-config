95848977ab59fb7cf9332ee2012ff3835c118819
Robin Bärtschi <rtmbaertschi007@gmail.com>
2025-02-08 15:42:21 +0100

    Generation 70 created at: 2025-02-08T14:42:18Z nixos: 25.05.20250207.a3eaf5e kernel: 6.12.12

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -22,6 +22,7 @@ with lib; {
               ".config/hypr/hyprland.conf".source = ../hypr/hyprland.conf;
               ".config/hypr/hypridle.conf".source = ../hypr/hypridle.conf;
               ".config/hypr/hyprpaper.conf".source = ../hypr/hyprpaper.conf;
+              ".config/hypr/wallpaper1.png".source = ../hypr/wallpaper1.png;
               ".config/fuzzel/fuzzel.ini".source = ../hypr/fuzzel.ini;
               ".config/hypr/ac.py".source = ../hypr/ac.py;
               ".config/waybar/".source = ../waybar;
