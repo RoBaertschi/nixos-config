b83b94a29223972a4227aa344a166ce50fde5abc
Robin Bärtschi <rtmbaertschi007@gmail.com>
2025-02-09 13:40:03 +0100

    Generation 77 created at: 2025-02-09T12:39:41Z nixos: 25.05.20250208.a79cfe0 kernel: 6.12.12

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -1,9 +1,12 @@
-{pkgs, config, lib, ...}:
-with lib;
-
-
-let cfg = config.tmux; in
 {
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+with lib; let
+  cfg = config.tmux;
+in {
   options = {
     imports = [./main-user.nix];
     tmux = {
@@ -14,29 +17,29 @@ let cfg = config.tmux; in
     home-manager = {
       users = {
         "${config.main-user.userName}" = {
-            programs.tmux = {
-              enable = true;
-              baseIndex = 1;
-              clock24 = true;
-              mouse = true;
-              prefix = "C-Space";
-              keyMode = "vi";
-              extraConfig = ''
-                bind -n M-H previous-window
-                bind -n M-L next-window
-                bind-key -T copy-mode-vi v send-keys -X begin-selection
-                bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
-                bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
-                bind '"' split-window -v -c "#{pane_current_path}"
-                bind % split-window -h -c "#{pane_current_path}"
-              '';
-              plugins = with pkgs; [
-                tmuxPlugins.sensible
-                tmuxPlugins.vim-tmux-navigator
-                tmuxPlugins.yank
-                tmuxPlugins.tokyo-night-tmux
-              ];
-            };
+          programs.tmux = {
+            enable = true;
+            baseIndex = 1;
+            clock24 = true;
+            mouse = true;
+            prefix = "C-Space";
+            keyMode = "vi";
+            extraConfig = ''
+              bind -n M-H previous-window
+              bind -n M-L next-window
+              bind-key -T copy-mode-vi v send-keys -X begin-selection
+              bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+              bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+              bind '"' split-window -v -c "#{pane_current_path}"
+              bind % split-window -h -c "#{pane_current_path}"
+            '';
+            plugins = with pkgs; [
+              tmuxPlugins.sensible
+              tmuxPlugins.vim-tmux-navigator
+              tmuxPlugins.yank
+              tmuxPlugins.tokyo-night-tmux
+            ];
+          };
         };
       };
     };
