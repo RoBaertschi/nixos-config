21a6fb44867adf1b8db1c92fd04f225ccac954e6
Robin Bärtschi <robin@nixos.>
2025-02-13 10:33:24 +0100

    WSL: Generation 8 created at: 2025-02-13T09:06:53Z nixos: 25.05.20250208.a79cfe0 kernel: Unknown

---
+++ edit-wsl.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+nvim .
+git add .
+sudo nixos-rebuild switch --flake /home/robin/nixos-config/#wsl
+COMMIT_MESSAGE="WSL: $(nixos-rebuild list-generations --json | jq --raw-output 'map(select(.current)) | .[0] | "Generation \(.generation)\ncreated at: \(.date)\nnixos: \(.nixosVersion)\nkernel: \(.kernelVersion)"')"
+git commit -m "$COMMIT_MESSAGE"
+git push
