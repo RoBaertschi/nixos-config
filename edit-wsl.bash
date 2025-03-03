b070bda8e8690c7df6c21617ec798f840791517f
Robin Bärtschi <rtmbaertschi007@gmail.com>
2025-03-03 15:45:51 +0100

    WSL: Generation 17 created at: 2025-03-03T14:45:50Z nixos: 25.05.20250301.303bd80 kernel: Unknown

--- edit-wsl.bash
+++ edit-wsl.bash
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
-nvim .
+TYPE="switch"
+
+if [ $1 = "boot" ]; then
+  TYPE="boot"
+fi
+
+if [ $TYPE != "boot" ]; then
+  nvim .
+fi
 git add .
-sudo nixos-rebuild switch --flake /home/robin/nixos-config/#wsl
+sudo nixos-rebuild $TYPE --flake /home/robin/nixos-config/#wsl
 COMMIT_MESSAGE="WSL: $(nixos-rebuild list-generations --json | jq --raw-output 'map(select(.current)) | .[0] | "Generation \(.generation)\ncreated at: \(.date)\nnixos: \(.nixosVersion)\nkernel: \(.kernelVersion)"')"
 git commit -m "$COMMIT_MESSAGE"
 git push
