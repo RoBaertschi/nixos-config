398d77cf9f231ca8992d9d5aca45bcacbcaf6e20
Robin Bärtschi <rtmbaertschi007@gmail.com>
2025-03-22 16:36:22 +0100

    Generation 135 created at: 2025-03-22T15:36:20Z nixos: 25.05.20250319.a84ebe2 kernel: 6.12.19

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -105,7 +105,7 @@
   programs.nix-ld = {
     enable = true;
     libraries = with pkgs; [
-      alsa-lib at-spi2-atk cairo cups.lib dbus.lib expat glib libdrm libgbm libxkbcommon nspr nss_latest pango xorg.libX11 xorg.libXcomposite xorg.libXdamage xorg.libXext xorg.libXfixes xorg.libXrandr xorg.libxcb xorg.libXcursor libGL xorg.libXi
+      alsa-lib at-spi2-atk cairo cups.lib dbus.lib expat glib libdrm libgbm libxkbcommon nspr nss_latest pango xorg.libX11 xorg.libXcomposite xorg.libXdamage xorg.libXext xorg.libXfixes xorg.libXrandr xorg.libxcb xorg.libXcursor libGL xorg.libXi glfw xorg.libX11
     ];
   };
 
