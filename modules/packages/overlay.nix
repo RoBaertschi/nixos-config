292b6548014c8624a6666678989fca86ee031a88
Robin Bärtschi <rtmbaertschi007@gmail.com>
2025-06-02 19:49:01 +0200

    Generation 196 created at: 2025-06-02T17:49:00Z nixos: 25.11.20250518.292fa7d kernel: 6.12.28

--- modules/packages/overlay.nix
+++ modules/packages/overlay.nix
@@ -1,10 +1,10 @@
 final: prev: {
   odin = prev.odin.overrideAttrs {
-    version = "dev-2025-04";
+    version = "dev-2025-06";
     src = prev.fetchFromGitHub {
       owner = "odin-lang";
       repo = "Odin";
-      rev = "dev-2025-04";
+      rev = "dev-2025-06";
       hash = "sha256-dVC7MgaNdgKy3X9OE5ZcNCPnuDwqXszX9iAoUglfz2k=";
     };
     buildInputs = [prev.sdl3];
