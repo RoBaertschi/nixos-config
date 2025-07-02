24e7d5c718bc400d5eac865d91cd2ae7eb8a3b87
Robin Bärtschi <rtmbaertschi007@gmail.com>
2025-07-02 18:08:35 +0200

    Generation 205 created at: 2025-07-02T16:08:34Z nixos: 25.11.20250607.3e3afe5 kernel: 6.12.32

--- modules/virt-manager.nix
+++ modules/virt-manager.nix
@@ -15,8 +15,7 @@
     users.groups.libvirtd.members = [config.main-user.userName];
     virtualisation.libvirtd.enable = true;
     virtualisation.spiceUSBRedirection.enable = true;
-    networking.firewall.enable = false;
-    # networking.firewall.trustedInterfaces = ["virbr0" "vnet0"];
+    networking.firewall.trustedInterfaces = ["virbr0" "vnet0"];
     home-manager.users."${config.main-user.userName}".dconf.settings = {
       "org/virt-manager/virt-manager/connections" = {
         autoconnect = ["qemu:///system"];
