358081bdc0994155be8ccb7a335fc2094358782a
Robin Bärtschi <rtmbaertschi007@gmail.com>
2025-07-02 18:15:48 +0200

    Generation 207 created at: 2025-07-02T16:15:48Z nixos: 25.11.20250607.3e3afe5 kernel: 6.12.32

--- modules/virt-manager.nix
+++ modules/virt-manager.nix
@@ -16,7 +16,8 @@
     virtualisation.libvirtd.enable = true;
     virtualisation.spiceUSBRedirection.enable = true;
     services.dnsmasq.enable = true;
-    services.resolved.enable = false;
+    networking.firewall.allowedTCPPorts = [53];
+    networking.firewall.allowedUDPPorts = [53];
     networking.firewall.trustedInterfaces = ["virbr0" "vnet0"];
     home-manager.users."${config.main-user.userName}".dconf.settings = {
       "org/virt-manager/virt-manager/connections" = {
